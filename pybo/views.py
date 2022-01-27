cbcd0af38d905d79364c070feb3dd40768803340
HyejiKim <hazel22kim@gmail.com>
2022-01-27 23:15:18 +0900

    third commit

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,38 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신걸 환영합니다.")
+    """
+    Pybo 목록 출력
+    :param request:
+    :return: 질문목록html
+    """
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
 
+
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    :param request:
+    :param question_id:
+    :return: 상세 내용 html
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
+
+
+def answer_create(request, question_id):
+    """
+    pybo 답변 등
+    :param request:
+    :param question_id:
+    :return: redirect 상세 내용 html
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'))
+    return redirect('pybo:detail', question_id=question.id)
