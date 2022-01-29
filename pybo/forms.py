f518851f9deed5d6f21b55f86ab04ab5737e5564
HyejiKim <hazel22kim@gmail.com>
2022-01-29 23:41:56 +0900

    study style, form

---
+++ pybo/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from pybo.models import Question, Answer 
+
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ['subject', 'content']
+        """
+        {{ form.as_p }} 태그 HTML코드를 자동으로 생성하므로 부트스트랩이나 부수적인 요소를 적용하기 위해서는 
+        아래와 같이 widget을 사용하면 된다. 그러나 보통 디자인적으로 제한이 많이 생기므로 수동으로 폼을 작성한다.(question_form.html참조)
+        """
+        # widgets = {
+        #     'subject': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),
+        # }
+        """
+        또한 모델의 컬럼명이 영문이 아닌 다른 언어로 표시하고 싶다면 labels속성을 지정해주면 된다.
+        """
+        labels = {
+            'subject': '제목',
+            'content': '내용',
+        }
+
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
+        labels = {
+            'content': '답변내용',
+        }
--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+
+from .forms import QuestionForm, AnswerForm
 from .models import Question
 
 
@@ -34,5 +36,31 @@ def answer_create(request, question_id):
     :return: redirect 상세 내용 html
     """
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'))
-    return redirect('pybo:detail', question_id=question.id)
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question_id)
+    else:
+        form = AnswerForm()
+    content = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', content)
+
+
+def question_create(request):
+    """
+    pybo 질문 등록
+    :param request: 
+    :return: 
+    """
+    if request.method == 'POST':
+        form = QuestionForm(request.POST) # request.POST 에는 사용자가 입력한 내용들이 담겨있다.
+        if form.is_valid():
+            form.save(commit=True) # QuestionForm이Question모델과 연결된 폼이므로 'form.save()'이렇게 사용 가능
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
